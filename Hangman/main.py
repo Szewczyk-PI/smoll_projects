a1437485029fe10a602c97e0e876265b3df2d139
Kuba <ksszewczykkuba@gmail.com>
2025-12-21 19:59:01 +0100

    wordking on Hangman game

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from hangman!")
+import random
+words = ["aardvark", "baboon", "camel"]
 
+word_pick = random.choice(words)
+print(word_pick)
 
-if __name__ == "__main__":
-    main()
+for word in word_pick:
+    liczba = len(word_pick)
+    print("_ " * liczba)
+    guess = input("Podaj literę: ").lower()
+    if (guess in word_pick):
+        print("True")
+    else:
+        print("False")
