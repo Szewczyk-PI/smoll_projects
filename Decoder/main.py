57fbfeb662f971365c6d67003f453264e1449ea1
Kuba <ksszewczykkuba@gmail.com>
2026-01-02 21:57:14 +0100

    add Decoder project

--- Decoder/main.py
+++ Decoder/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from decoder!")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-if __name__ == "__main__":
-    main()
+def cesar(direction, text, shift):
+    if direction == 'encode':
+        cipher_text = ""
+        for letter in text:
+            shifted_position = alphabet.index(letter) + shift
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+        print(f"Here is the encoded result: {cipher_text}")
+    elif direction == 'decode':
+        text1 = ""
+        for letter in text:
+            shifted_position = alphabet.index(letter) - shift
+            shifted_position %= len(alphabet)
+            text1 += alphabet[shifted_position]
+        print(f"Here is the decoded result: {text}")
+    else:
+        print("Wrong Input")
+
+cesar(direction, text, shift)
