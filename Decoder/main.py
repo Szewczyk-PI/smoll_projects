3c8ed77e94befd23425a73b958873e0d3b54e230
Kuba <ksszewczykkuba@gmail.com>
2026-01-03 22:56:04 +0100

    end Decoder project

--- Decoder/main.py
+++ Decoder/main.py
@@ -1,26 +1,37 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
 
-def cesar(direction, text, shift):
-    if direction == 'encode':
-        cipher_text = ""
-        for letter in text:
-            shifted_position = alphabet.index(letter) + shift
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+        if letter in alphabet:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
+
+            shifted_position = alphabet.index(letter) + shift_amount
             shifted_position %= len(alphabet)
-            cipher_text += alphabet[shifted_position]
-        print(f"Here is the encoded result: {cipher_text}")
-    elif direction == 'decode':
-        text1 = ""
-        for letter in text:
-            shifted_position = alphabet.index(letter) - shift
-            shifted_position %= len(alphabet)
-            text1 += alphabet[shifted_position]
-        print(f"Here is the decoded result: {text}")
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+repeat = True
+while repeat:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    odp = input("Type 'yes' if you want to go again. Otherwise, type 'no'.")
+    if odp == "no":
+        repeat = False
     else:
-        print("Wrong Input")
+        continue
 
-cesar(direction, text, shift)
+
+
+
